5be7a2f5ef0cf0c1ae9ca3ecf742f19b0a5c436a
hyeonjong lee <88295516+qhtneo@users.noreply.github.com>
2024-11-12 20:20:52 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 104 ms, Memory: 109240 KB -BaekjoonHub

---
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
@@ -0,0 +1,26 @@
+N = int(input())
+num_list = list(map(int,input().split()))
+
+# lis의 끝 원소들을 저장할 리스트
+lis = []
+
+for num in num_list:
+  # 현재 원소의 위치를 직접 구현한 이진 탐색으로 찾기
+  left, right = 0, len(lis)
+  while left < right:
+    mid = (left + right) // 2
+    if lis[mid] < num:
+      left = mid + 1
+    else:
+      right = mid
+  pos = left
+
+# 위치가 리스트의 길이와 같으면 새로운 원소 추가
+  if pos == len(lis):
+    lis.append(num)
+  else:
+    # 기존 원소를 현재 원소로 교체
+    lis[pos] = num
+    
+# 최장 증가 부분 수열의 길이 반환
+print(len(lis))
